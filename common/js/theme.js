b0d9e7853aad90a9be72a174a3bc8c24f437c3ae
dungptit1410 <lavandung1998@gmail.com>
2020-08-19 16:14:06 +0700

    gymmax update

---
+++ common/js/theme.js
@@ -0,0 +1,16 @@
+
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "280px";
+  var x = document.getElementsByTagName("BODY")[0];
+  // x.style.backgroundColor = "black";
+  // x.style.opacity = "0.4";
+  // var y = document.getElementsByClassName("head__menu-responsive");
+  
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+  var x = document.getElementsByTagName("BODY")[0];
+  // x.style.backgroundColor = "none";
+  // x.style.opacity = "1";
+}
